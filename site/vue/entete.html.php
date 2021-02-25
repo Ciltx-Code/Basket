cc48c299168a87700d992f4354d54ea98631e4a8
Garlaschi Lucas <lucas.garlaschi@yahoo.com>
2021-02-25 22:22:21 +0100

    ajout page accueil stylé

--- site/vue/entete.html.php
+++ site/vue/entete.html.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-	<title>Basket</title>
-	<style type="text/css">
-		@import url("css/hello.css");
-	</style>
-	<link rel="icon" href="../site/images/icone.png" />
-	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
-</head>
 <body>
-	<nav>
-		<ul>
-			<li><a href="../site">Accueil</a></li>
-			<li><a href="">Arbitres</a></li>
-			<li><a href="">Catégorie</a></li>
-			<li><a href="">Connexion</a></li>
-		</ul>
-	</nav>
+	<div class="entete">
+		<img src="../site/images/ffbb_icone.jpg">
+
+		<nav>
+			<ul>
+				<li><a href="../site">Accueil</a></li>
+				<li><a href="">Arbitres</a></li>
+				<li><a href="">Catégorie</a></li>
+				<li><a href="">Connexion</a></li>
+			</ul>
+		</nav>
+	</div>
--- site/vue/vueAccueil.php
+++ site/vue/vueAccueil.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+	<title>FFBB - Accueil</title>
+	<style type="text/css">
+		@import url("css/hello.css");
+	</style>
+	<link rel="icon" href="../site/images/icone.png" />
+	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+</head>
 <?php 
 include "./vue/entete.html.php";
 ?>
@@ -9,3 +20,5 @@ include "./vue/accueil.html.php"
 <?php 
 include "./vue/pied.html.php";
 ?>
+</body>
+</html>
