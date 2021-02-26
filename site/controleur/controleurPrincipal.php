9e3d5ff7f5be63695ca3f3ae3fc3b22580e08b6b
Lucas Garlaschi <lucas.garlaschi@yahoo.com>
2021-02-26 16:58:04 +0100

    mort2

--- site/controleur/controleurPrincipal.php
+++ site/controleur/controleurPrincipal.php
@@ -4,7 +4,7 @@ function controleurPrincipal($action){
     $lesActions = array();
     $lesActions["defaut"] = "hello.php";
     $lesActions["hello"] = "hello.php";
-
+    $lesActions["categorie"] = "GotoCategorie.php";
     
     if (array_key_exists ( $action , $lesActions )){
         return $lesActions[$action];
