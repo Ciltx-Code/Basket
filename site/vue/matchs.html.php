ee2daf484fe8073a243b61543963c75c7782a344
Garlaschi Lucas <lucas.garlaschi@yahoo.com>
2021-03-15 16:51:50 +0100

    on est laaaaa

--- site/vue/matchs.html.php
+++ site/vue/matchs.html.php
@@ -5,16 +5,24 @@
         <table> 
         <tr>
         <td></td> 
-        <td>Adresse de la salle</td>
-        <td>Date du match</td>
-        <td>Heure du match</td>
-        <td>Montant Déplacement Arbitre 1</td>
-        <td>Montant Déplacement Arbitre 2</td></tr>";
+        <td>Adresse</td>
+        <td>Date</td>
+        <td>Heure</td>
+        <td>Equipe 1</td>
+        <td>Equipe 2</td>
+        <td>Arbitre 1</td>
+        <td>Montant</td>
+        <td>Arbitre 2</td>
+        <td>Montant</td></tr>";
 
         $listeMatchs = getMatchs();
         $count = 0;
         while($ligne = $listeMatchs -> fetch(PDO::FETCH_OBJ)){
             $numsalle = getSalleByAdresse($ligne->adresse_salle);
+            $equipe1 = getEquipeById($ligne->num_equipe_1);
+            $equipe2 = getEquipeById($ligne->num_equipe_2);
+            $arbitre1 = getArbitreById($ligne->num_arbitre_p);
+            $arbitre2 = getArbitreById($ligne->num_arbitre_s);
             echo "<tr>";?>
             <td>
                 <input type='radio' name='select' 
@@ -26,6 +34,10 @@
             echo "<td>$ligne->adresse_salle</td>";
             echo "<td>$ligne->date_match</td>";
             echo "<td>$ligne->heure_match</td>";
+            echo "<td>$equipe1</td>";
+            echo "<td>$equipe2</td>";
+            echo "<td>$arbitre1</td>";
+            echo "<td>$arbitre2</td>";
             echo "<td>$ligne->montant_déplt_p</td>";
             echo "<td>$ligne->montant_déplt_s</td>";
             echo "</tr>";
@@ -42,9 +54,7 @@
         <tr>
         <td>Adresse de la salle</td>
         <td>Date du match</td>
-        <td>Heure du match</td>
-        <td>Montant Déplacement Arbitre 1</td>
-        <td>Montant Déplacement Arbitre 2</td></tr>";
+        <td>Heure du match</td>";
 
         $listeMatchs = getMatchs();
 
@@ -55,8 +65,6 @@
             echo "<td>$ligne->adresse_salle</td>";
             echo "<td>$ligne->date_match</td>";
             echo "<td>$ligne->heure_match</td>";
-            echo "<td>$ligne->montant_déplt_p</td>";
-            echo "<td>$ligne->montant_déplt_s</td>";
             echo "</tr>";
         }
         echo "</table></div>";
