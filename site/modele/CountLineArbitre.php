777907e5d0ff115a1ea27e9e2ec70e733e5fd297
Théo <theo.gaming@outlook.fr>
2021-03-25 17:19:23 +0100

    CountLineV2

--- site/modele/CountLineArbitre.php
+++ site/modele/CountLineArbitre.php
@@ -3,17 +3,13 @@ try{
     include "../modele/bd_connexion.php";
     $cnx = connexionPDO();
 
-    $nom = $_POST['nom'];
-    $equipe1 = $_POST['equipe1'];
-    $equipe2 = $_POST['equipe2'];
-
     $req = $cnx->prepare("Select count(num_arbitre) as ligneArbitre from arbitre");
     $req->execute();
     $ligne = $req->fetch(PDO::FETCH_OBJ);
     $compteTotal = $ligne->ligneArbitre;
 
 
-    echo'$compteTotal';
+    echo $compteTotal;
 
 
 }catch (PDOException $e) {
